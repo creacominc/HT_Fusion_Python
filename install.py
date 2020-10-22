12e9a6d834009b70fa168a075476008c4da20f8d
Harold Tomlinson <haroldt@paralandra5.myfiosgateway.com>
2020-10-22 18:22:53 -0400

    fixed the copy.  copytree does not work well enough in 3.6

--- install.py
+++ install.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import os
+import shutil
 
-FUSION_COMP_FOLDER = "~/Library/Application Support/Blackmagic Design/Fusion/Scripts"
+FUSION_COMP_FOLDER = os.path.expanduser( "~/Library/Application Support/Blackmagic Design/Fusion/Scripts" )
+# os.chdir( 'Comp' )
+# print(  FUSION_COMP_FOLDER )
+# print( shutil.copytree( '.', FUSION_COMP_FOLDER, symlinks=True ) )
 
 # find all the files under Comp and copy them to the library folder
 for subdir, dirs, files in os.walk( 'Comp' ):
@@ -10,3 +14,4 @@ for subdir, dirs, files in os.walk( 'Comp' ):
         if filepath.endswith(".py") :
             destination = FUSION_COMP_FOLDER + os.sep + os.path.relpath(filepath, 'Comp/')
             print ( f'copy {filepath} to {destination}')
+            shutil.copyfile( filepath, destination )

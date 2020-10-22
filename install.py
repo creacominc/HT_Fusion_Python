1a42a64e225702160d8f43661c960bf16f6fa0d9
Harold Tomlinson <haroldt@paralandra5.myfiosgateway.com>
2020-10-22 18:04:32 -0400

    setting up dev environment for Fusion Scripting (python)

---
+++ install.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+
+FUSION_COMP_FOLDER = "~/Library/Application Support/Blackmagic Design/Fusion/Scripts"
+
+# find all the files under Comp and copy them to the library folder
+for subdir, dirs, files in os.walk( 'Comp' ):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+        if filepath.endswith(".py") :
+            destination = FUSION_COMP_FOLDER + os.sep + os.path.relpath(filepath, 'Comp/')
+            print ( f'copy {filepath} to {destination}')

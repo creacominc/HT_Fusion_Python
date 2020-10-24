ce3a80720d52d2e032c015e282f71a6a6159a073
creacominc <haroldt@creacom.ca>
2020-10-23 20:58:13 -0400

    not quite right yet.  the TOOLI_ImageWidth is the size last processed, not the size.

--- Comp/Comp/changeCompToLowRes.py
+++ Comp/Comp/changeCompToLowRes.py
@@ -204,6 +204,26 @@ def reportOnAllTools( comp ) :
         # reportOnToolData( tool )
         pprint( f" ^^^^^^^^  end for {tool.Name}" )
 
+def changeAllTools( comp ) :
+    # update every tool in the composition
+    for tool in comp.GetToolList().values() :
+        pprint( '' )
+        pprint( f'Tool ID: {tool.ID},  Name: {tool.Name}' )
+        # Some Attributes will not be visible until the tool is shown in one of the windows.
+        # Select and show this tool in window 1
+        comp.SetActiveTool( tool )
+        comp.CurrentFrame.ViewOn( tool, 1 )
+        #
+        toolAttrs = tool.GetAttrs()
+        #pprint( f" --------- Tool attributes: {toolAttrs}" )
+        if 'TOOLI_ImageWidth' in toolAttrs :
+            pprint( f'               imageWidth:   {tool.GetAttrs("TOOLI_ImageWidth")}' )
+            pprint( f'               imageHeight:  {tool.GetAttrs("TOOLI_ImageHeight")}' )
+        tool.SetAttrs( {"TOOLI_ImageWidth":formats['LowRes']['Width'], "TOOLI_ImageHeight":formats['LowRes']['Height']} )
+        pprint( f'               imageWidth:   {tool.GetAttrs("TOOLI_ImageWidth")}' )
+        pprint( f'               imageHeight:  {tool.GetAttrs("TOOLI_ImageHeight")}' )
+        pprint( f" ^^^^^^^^  end for {tool.Name}" )
+
 
 
 def reportOnComp( comp ) :
@@ -395,31 +415,43 @@ def reportOnComp( comp ) :
     rate   = compPrefsFormat['Rate']
     print( f"             Config - name: {name}, height: {height}, width: {width}, rate: {rate}")
 
-def changeDefault() :
-    comp = fu.GetCurrentComp()
-    compPrefsFormat = comp.GetPrefs()['Comp']['FrameFormat']
+def changeDefault( comp ) :
+    '''
+    set the default for the composition to low res so that added tools get the low res setting
+    '''
+    compAttrs = comp.GetAttrs()
+    pprint( '' )
+    # report the name and file
+    compName = compAttrs['COMPS_Name'].strip()
+    compFile = compAttrs['COMPS_FileName'].strip()
+    pprint( f'Composition Name: {compName}' )
+    pprint( f'            Path: {compFile}' )
+    # report the current format
+    compPrefs = comp.GetPrefs()
+    compPrefsFormat = compPrefs['Comp']['FrameFormat']
     name = compPrefsFormat['Name']
     height = compPrefsFormat['Height']
     width  = compPrefsFormat['Width']
     rate   = compPrefsFormat['Rate']
-    print( f"Current config - name: {name}, height: {height}, width: {width}, rate: {rate}")
+    print( f"            Current config - name: {name}, height: {height}, width: {width}, rate: {rate}")
     if ( name != formats['LowRes']['Name'] ):
-        print( f"replacing global preference with {formats['LowRes']['Name']}" )
-        # rc = comp.SetPrefs( 
-        #     {
-        #         "Comp.FrameFormat.GuidRatio": formats['LowRes']['GuideRatio'],
-        #         "Comp.FrameFormat.Height": formats['LowRes']['Height'],
-        #         "Comp.FrameFormat.Name": formats['LowRes']['Name'],
-        #         "Comp.FrameFormat.Rate": formats['LowRes']['Rate'],
-        #         "Comp.FrameFormat.Width": formats['LowRes']['Width']
-        #     }
-        # )
-    changeAllTools()
+        print( f"Replacing global preference with {formats['LowRes']['Name']}" )
+        rc = comp.SetPrefs(
+            {
+                "Comp.FrameFormat.GuidRatio": formats['LowRes']['GuideRatio'],
+                "Comp.FrameFormat.Height": formats['LowRes']['Height'],
+                "Comp.FrameFormat.Name": formats['LowRes']['Name'],
+                "Comp.FrameFormat.Rate": formats['LowRes']['Rate'],
+                "Comp.FrameFormat.Width": formats['LowRes']['Width']
+            }
+        )
 
 def main() :
     comp = fu.GetCurrentComp()
     reportOnComp( comp )
     reportOnAllTools( comp )
+    #changeDefault( comp )
+    #changeAllTools( comp )
 
 
 if __name__ == "__main__" :
